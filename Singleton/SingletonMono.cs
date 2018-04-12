2f14854c7caabe3cb03d16dac8179ebbbeea429a
黄早艺 <1037725467@qq.com>
2018-04-12 22:50:24 +0800

    分类

---
+++ Singleton/SingletonMono.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour,new()
+{
+    private static volatile T instance = null;
+    private static readonly object syncLock = new object();
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new T();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
